c56217ee89224598b39a9b8242be4d6b8f3f9b0e
fubao <freadream@gmail.com>
2017-08-27 23:47:01 -0400

    update

--- getTopic.py
+++ getTopic.py
@@ -10,13 +10,13 @@ Created on Sun Aug 27 11:53:12 2017
 #first focus on the data management conference here 
 
 from os import listdir
-from os.path import isfile, join
+from os.path import join
 import pandas as pd
 
 class confTopicClass(object):
    
     #abbreviation name --> to topic edge list
-    conferenceWebsites = {}
+    conferenceNameToTopicEdgeLst = []
     
     
     def __init__(self):
@@ -30,18 +30,20 @@ class confTopicClass(object):
             #print ("filePath: ", filePath)
             self.readEachTopicFile(filePath)
         
-        print ("len readEachTopicFile: ", len(confTopicClass.conferenceWebsites))
+        print ("len readEachTopicFile: ", len(confTopicClass.conferenceNameToTopicEdgeLst))
+        
+    #read every stored topic file
     def readEachTopicFile(self, fileIn):
         df = pd.read_csv(fileIn, delimiter = '\t')
         print (df.columns[0])
        #print (df.values)
         abbreName = df.columns[0].split('-')[0].lower().strip()
         for val in df.values:
-            if abbreName not in confTopicClass.conferenceWebsites:
-                confTopicClass.conferenceWebsites[abbreName] = val
+                edgeProp = "same"
+                confTopicClass.conferenceNameToTopicEdgeLst.append([abbreName, val, edgeProp])
+                confTopicClass.conferenceNameToTopicEdgeLst.append([val, abbreName, edgeProp])
         
-        
-    
+
     
 
 def main():
@@ -51,8 +53,6 @@ def main():
     filePaths = "/home/fubao/workDir/ResearchProjects/GraphQuerySearchRelatedPractice/Data/dblpParserGraph/input/conferenceTopicFile/"
     confTopicObj.readConfTopics(filePaths)
     
-    
-
     
     
 if __name__== "__main__":
